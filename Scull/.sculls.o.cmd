0360a4f0216010aeef72be875a9ed670954359dc
wangforthinker <924701889@qq.com>
2014-05-07 21:07:57 +0800

    update

--- Scull/.sculls.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/wang/code/Scull/sculls.o := ld -m elf_x86_64   -r -o /home/wang/code/Scull/sculls.o /home/wang/code/Scull/scull.o /home/wang/code/Scull/scull_set.o 
--- Scull/.tmp_versions/sculls.mod
+++
@@ -1,2 +0,0 @@
-/home/wang/code/Scull/sculls.ko
-/home/wang/code/Scull/scull.o /home/wang/code/Scull/scull_set.o
